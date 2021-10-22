0427c345785c669e6087014470bfabad12d833b6
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2021-10-22 12:01:09 -0400

    dropping incorrect styles in favor of bootstrap

---
+++ tests/dummy/app/controllers/application.ts
@@ -0,0 +1,11 @@
+import Controller from '@ember/controller';
+import { set } from '@ember/object';
+
+
+export default class ApplicationController extends Controller {
+  public shrinkJumbotron = false;
+
+  toggleJumbotron() {
+    set(this, 'shrinkJumbotron', !this.shrinkJumbotron);
+  }
+}
