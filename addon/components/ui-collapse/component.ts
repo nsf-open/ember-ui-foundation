f15b8149bb1324cd1ecb531a67280834378351bf
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-01-31 08:44:24 -0500

    docs: ui-collapse

--- addon/components/ui-collapse/component.ts
+++ addon/components/ui-collapse/component.ts
@@ -11,77 +11,52 @@ import { waitForTransitionEnd } from '../../utils';
 import { Dimensions } from '../../constants';
 import template from './template';
 
+type VoidCallback = () => void;
+
 /**
  * The UiCollapse component provides the base mechanics to smoothly collapse/expand a block element.
- *
- * @class UiCollapse
  */
 @classNames('ui-collapse')
 @layout(template)
 export default class UiCollapse extends Component {
   /**
    * Toggles whether the element is expanded or collapsed.
-   *
-   * @property collapsed
-   * @type {boolean}
    */
   public collapsed = true;
 
   /**
-   * An optional callback that will be ran when the element first begins to collapse.
-   *
-   * @property onHide
-   * @type {() => void}
+   * An optional callback that will run when the element first begins to collapse.
    */
-  public onHide?: () => void;
+  public onHide?: VoidCallback;
 
   /**
-   * An optional callback that will be ran when the element finishes being collapsed.
-   *
-   * @property onHidden
-   * @type {() => void}
+   * An optional callback that will run when the element finishes being collapsed.
    */
-  public onHidden?: () => void;
+  public onHidden?: VoidCallback;
 
   /**
-   * An optional callback that will be ran when the element first begins to expand.
-   *
-   * @property onShow
-   * @type {() => void}
+   * An optional callback that will run when the element first begins to expand.
    */
-  public onShow?: () => void;
+  public onShow?: VoidCallback;
 
   /**
-   * An optional callback that will be ran when the element finishes being expanded.
-   *
-   * @property onShown
-   * @type {() => void}
+   * An optional callback that will run when the element finishes being expanded.
    */
-  public onShown?: () => void;
+  public onShown?: VoidCallback;
 
   /**
    * The axis that the collapse/expand will occur on. Can be either "height" or "width".
-   *
-   * @property collapseDimension
-   * @type {Dimensions}
-   * @default "height"
    */
   @className
   public collapseDimension = Dimensions.Height;
 
   /**
    * This can be set to a value > 0 if a custom expanded height/width pixel value is needed.
-   *
-   * @property expandedSize
-   * @type {number}
    */
   public expandedSize?: number;
 
   /**
    * This can be set to a value > 0 if a custom collapsed height/width pixel value is needed.
-   *
-   * @property collapsedSize
-   * @type {number}
    */
   public collapsedSize = 0;
 
@@ -90,38 +65,51 @@ export default class UiCollapse extends Component {
    * in order to provide a destination value for the CSS transition to move to. The inline
    * value is typically removed when no longer needed but if you want it to keep it around
    * for whatever reason you can set this to false.
-   *
-   * @property resetSizeBetweenTransitions
-   * @type {boolean}
    */
   public resetSizeBetweenTransitions = true;
 
-  // Whether or not the component is actively undergoing a transition.
+  /**
+   * Whether the component is actively undergoing a transition.
+   *
+   * @protected
+   */
   @className('collapsing')
   transitioning = false;
 
-  // The calculated size for either width or height that the element will have when
-  // the transition is finished. This gets written as an inline style value for the
-  // element.
+  /**
+   * The calculated size for either width or height that the element will have when
+   * the transition is finished. This gets written as an inline style value for the
+   *  element.
+   *
+   * @protected
+   */
   transitioningSize?: number;
 
-  // Most likely, the "transitionend" event will be used to indicate that the
-  // collapse/expand has finished. This is a backup timeout if for whatever
-  // reason that doesn't happen.
+  /**
+   * Most likely, the "transitionend" event will be used to indicate that the
+   * collapse/expand has finished. This is a backup timeout if for whatever
+   * reason that doesn't happen.
+   *
+   * @protected
+   */
   transitionDuration = 350;
 
-  // An internal flag that will always be the opposite value of collapsed, so, it will
-  // be true if the element is expanded and false otherwise. The change occurs on the
-  // leading edge of the animation.
+  /**
+   * An internal flag that will always be the opposite value of collapsed, so, it will
+   * be true if the element is expanded and false otherwise. The change occurs on the
+   * leading edge of the animation.
+   *
+   * @protected
+   */
   active = false;
 
   @className('collapse')
   @not('transitioning')
-  declare notTransitioning: boolean;
+  protected declare notTransitioning: boolean;
 
   @className('show')
   @and('notTransitioning', 'active')
-  declare showContent: boolean;
+  protected declare showContent: boolean;
 
   @attribute
   @computed('collapseDimension', 'transitioningSize')
---
+++ addon/components/ui-collapse/ui-collapse.stories.ts
@@ -0,0 +1,53 @@
+import { hbs } from 'ember-cli-htmlbars';
+import { set } from '@ember/object';
+
+export default {
+  title: 'Elements/ui-collapse',
+  component: 'UiCollapse',
+};
+
+const Template = (context: unknown) => ({
+  context: Object.assign({}, context, {
+    toggleState() {
+      // @ts-expect-error - being added by the outer context
+      set(this, 'collapsed', !this.collapsed);
+    },
+  }),
+
+  // language=handlebars
+  template: hbs`
+    <UiButton
+      @text={{if this.collapsed "Expand" "Collapse"}}
+      @variant="default"
+      @onClick={{action this.toggleState}}
+    />
+
+    <hr />
+
+    <UiCollapse
+      @onShow={{action this.onShow}}
+      @onShown={{action this.onShown}}
+      @onHide={{action this.onHide}}
+      @onHidden={{action this.onHidden}}
+      @collapsed={{this.collapsed}}
+      @collapseDimension={{this.collapseDimension}}
+      @collapsedSize={{this.collapsedSize}}
+      @expandedSize={{this.expandedSize}}
+      @resetSizeBetweenTransitions={{this.resetSizeBetweenTransitions}}
+    >
+      <div style="background-color: #aaa; padding: 15px;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pretium luctus eros,
+            in semper arcu aliquet ut. Pellentesque eleifend diam et luctus placerat. Morbi varius
+            nec orci eu aliquet. In non pellentesque diam. Fusce dictum placerat enim. Aenean
+            vestibulum urna ut ante ullamcorper dapibus quis porta ante. Maecenas accumsan neque
+            nec scelerisque tristique. Curabitur cursus lectus tellus, sit amet ultrices libero
+            scelerisque eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia curae; Aenean sed bibendum libero. Aliquam in consequat augue, id volutpat
+            dui. Maecenas efficitur ultrices vulputate. Praesent quis turpis eu mauris consequat
+            laoreet.</p>
+      </div>
+    </UiCollapse>
+  `,
+});
+
+export const Default = Template.bind({});
