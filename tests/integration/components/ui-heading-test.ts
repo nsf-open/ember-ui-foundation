c9057713beb52e952158843012d9f09a694285ed
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-02-04 15:03:09 -0500

    feat: shrink ui-heading template with element helper and support block content

--- tests/integration/components/ui-heading-test.ts
+++ tests/integration/components/ui-heading-test.ts
@@ -33,4 +33,31 @@ module('Integration | Component | ui-heading', function (hooks) {
     this.set('level', HeadingLevels.H6);
     assert.dom('h6').hasText('Hello World');
   });
+
+  test('it will render the heading levels H1 - H6 with a content block', async function (assert) {
+    this.set('level', HeadingLevels.H1);
+
+    // language=handlebars
+    await render(
+      hbs`<UiHeading @level={{this.level}} @class="a-heading-class">Hello World</UiHeading>`
+    );
+
+    assert.dom('h1').hasText('Hello World');
+    assert.dom('h1').hasClass('a-heading-class');
+
+    this.set('level', HeadingLevels.H2);
+    assert.dom('h2').hasText('Hello World');
+
+    this.set('level', HeadingLevels.H3);
+    assert.dom('h3').hasText('Hello World');
+
+    this.set('level', HeadingLevels.H4);
+    assert.dom('h4').hasText('Hello World');
+
+    this.set('level', HeadingLevels.H5);
+    assert.dom('h5').hasText('Hello World');
+
+    this.set('level', HeadingLevels.H6);
+    assert.dom('h6').hasText('Hello World');
+  });
 });
