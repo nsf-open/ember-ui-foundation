8a330e17ebc6dee96606868d7f5924744d0b4f15
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-03-24 10:25:10 -0400

    test: add setters to dummy app controllers so tests can set breadcrumbs

--- tests/dummy/app/ui/artists/artist/album/controller.ts
+++ tests/dummy/app/ui/artists/artist/album/controller.ts
@@ -1,12 +1,22 @@
 import type { AlbumRouteModel } from 'dummy/ui/artists/artist/album/route';
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, set } from '@ember/object';
 
 export default class AlbumController extends Controller {
   declare readonly model: AlbumRouteModel;
 
+  _crumb?: unknown;
+
   @computed('model.name')
   get breadCrumbs() {
+    if (this._crumb) {
+      return this._crumb;
+    }
+
     return [{ label: 'Albums', path: 'artists.artist.discography' }, { label: this.model?.name }];
+  }
+
+  set breadCrumbs(value: unknown) {
+    set(this, '_crumb', value);
   }
 }
--- tests/dummy/app/ui/artists/artist/controller.ts
+++ tests/dummy/app/ui/artists/artist/controller.ts
@@ -1,12 +1,22 @@
 import type { ArtistRouteModel } from 'dummy/ui/artists/artist/route';
 import Controller from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, set } from '@ember/object';
 
 export default class ArtistController extends Controller {
   declare readonly model: ArtistRouteModel;
 
-  @computed('model.{id,name}')
+  _crumb?: unknown;
+
+  @computed('model.{id,name}', '_crumb')
   get breadCrumb() {
+    if (this._crumb) {
+      return this._crumb;
+    }
+
     return { label: this.model?.name, model: this.model?.id };
+  }
+
+  set breadCrumb(value: unknown) {
+    set(this, '_crumb', value);
   }
 }
