96cf9febf7f98fbf2d18774ffb3c243f8b211191
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-12-28 13:26:22 -0500

    docs: add positioning-root to SB preview
    this keeps certain elements like modals and popovers from having their positioned components rendered at the document body and so outside of the "root" element that Ember uses for event delegation.

--- storybook-utilities/static/js/preview-body.js
+++ storybook-utilities/static/js/preview-body.js
@@ -1,14 +1,4 @@
 (function () {
-  // This disables the bypassing of CSS transition aware promises
-  // that speed up testing.
+  // This disables the bypassing of CSS transition aware promises that speed up testing.
   window.__WAIT_FOR_TRANSITION_END__ = true;
-
-  document.addEventListener('DOMContentLoaded', function () {
-    const rootEl = document.getElementById('root');
-
-    if (rootEl) {
-      rootEl.classList.add('container');
-      rootEl.style.paddingTop = '15px';
-    }
-  });
 })();
